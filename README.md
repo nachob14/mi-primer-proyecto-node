1ecf166503d5e9a84f9dccd514c7093b64b23dfe
nachob14 <98466036+nachob14@users.noreply.github.com>
2023-01-23 11:39:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mi-primer-proyecto-node
+ Este es mi primer tarea para el curso de javascript de open bootcamp
---
+++ index.js
@@ -0,0 +1,3 @@
+// Este es el saludo indicado
+
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
